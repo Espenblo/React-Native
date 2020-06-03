f2eed98584d22138714ae777bcfcea5f8c42e047
Espenblo <espen.lovhaugen@gmail.com>
2020-06-03 18:47:20 +0200

    new

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hei</Text>
+      <Text>Open up App.js to start working on your app! Hei
+        La oss prøve
+      </Text>
     </View>
   );
 }
