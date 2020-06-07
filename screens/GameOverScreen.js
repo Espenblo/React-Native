533f109ea231a86c56a98e76b8566fed50a60dbe
Espenblo <espen.lovhaugen@gmail.com>
2020-06-08 00:21:43 +0200

    Updated fonts and img

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, Image } from 'react-native';
+import BodyText from '../components/BodyText';
 import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is over</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
-            <Button title="NEW GAME" onPress={props.onRestart}/>
+            <BodyText style={styles.title}>The Game is over</BodyText>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require('../assets/original.png')}
+                    // source={{uri: 'https://www.success.com/wp-content/uploads/2019/12/How-to-Align-Your-Career-With-Your-Personal-Definition-of-Success-1024x682.jpg'}}
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
+            <BodyText style={styles.resultText}> 
+                Your phone needed 
+                <Text style={styles.highlight}> {props.roundsNumber} </Text>
+                rounds to guess the number
+                <Text style={styles.highlight}> {props.userNumber} </Text>
+            </BodyText>
+            <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     );
 
@@ -19,6 +32,33 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: 'center',
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 28,
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold',
+        textAlign: 'center',
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 15,
+        marginVertical: 15,
     }
 });
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,6 +5,7 @@ import Card from '../components/Card';
 import Input from '../components/Input';
 import Colors from '../constants/colors';
 import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
 
 
 const StartGameScreen = props => {
@@ -43,7 +44,7 @@ const StartGameScreen = props => {
     if (confirmed) {
         confirmedOutput = (
             <Card style={styles.summaryContainer}>
-                <Text>You selected</Text>
+                <BodyText>You selected</BodyText>
                 <NumberContainer>{selectedNumber}</NumberContainer>
                 <Button title= "START GAME!" color={Colors.primary} onPress={()=> props.onStartGame(selectedNumber)}/>
             </Card>
@@ -56,7 +57,7 @@ const StartGameScreen = props => {
             <View style={styles.screen}>
                 <Text style={styles.title}>Start a New Game!</Text>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input style={styles.input}
                         blurOnSubmit autoCapitalize='none'
                         autoCorrect={false}
@@ -90,6 +91,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginVertical: 10,
+        fontFamily: 'open-sans-bold',
     },
 
     inputContainer: {
